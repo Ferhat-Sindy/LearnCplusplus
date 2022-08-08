5b69352618c50a65f2f114731ce0e0cd71ad427d
Ferhat <61943812+Ferhat-Sindy@users.noreply.github.com>
2022-08-08 13:21:24 +0200

    small update

---
+++ Chapter_1_tutorials/Simple_in_output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{   int pin;
+
+    std::cout << "type in your PIN and press return\n";
+    std::cin >> pin;
+
+    std::cout << "Your PIN is " << pin << "\n";
+    return 0;
+}
