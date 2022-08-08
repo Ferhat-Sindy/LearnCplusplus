03ef7167e122f6c255f00388ec7a2a4c43e7a58e
Ferhat <61943812+Ferhat-Sindy@users.noreply.github.com>
2022-08-08 12:45:36 +0200

    small update

--- Chapter_1_tutorials/BasicNumVar.cpp
+++ Chapter_1_tutorials/BasicNumVar.cpp
@@ -2,8 +2,18 @@
 
 int main(int argc,char* arg[])
 {
-    double x = 1.0, y = 1.0, u = 3.4, z;
+    int f =2, g=3 ;
+    double x = 1.0, y = 1.0, u = 3.4, z,h;
+
     z = fmod(x,y);
+
+    z++;
+
+    h = (double(f))/(double(g));
+
+    double array1[4] = {1,1,1,1};
+
+    double array2[2][2] = {{1,0},{0,1}};
 
     return 0;
 }
