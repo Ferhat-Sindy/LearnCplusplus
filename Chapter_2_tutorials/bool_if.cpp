0317bf8fc72b11040a65149101e530c6c323bad5
Ferhat <61943812+Ferhat-Sindy@users.noreply.github.com>
2022-08-13 21:44:47 +0200

    small update

---
+++ Chapter_2_tutorials/bool_if.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    bool flag = true;
+    if (flag){
+        std::cout << "This will be printed. \n";
+    }
+    else{
+        std::cout << "This won't be printed";
+    }
+}
