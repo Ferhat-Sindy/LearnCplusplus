2d16a09be0c85be147dc377dd85151c918e41648
Ferhat <61943812+Ferhat-Sindy@users.noreply.github.com>
2022-08-14 12:04:39 +0200

    small update

---
+++ Chapter_2_tutorials/for_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{   
+    double x = 10.0;
+    int count;
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "i: "<< i <<"\n";
+    }
+
+}
