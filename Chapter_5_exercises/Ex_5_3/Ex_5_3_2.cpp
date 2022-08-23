160fe98f3dc316975366037d93f33043c3c54807
Ferhat <61943812+Ferhat-Sindy@users.noreply.github.com>
2022-08-23 17:50:50 +0200

    small update

---
+++ Chapter_5_exercises/Ex_5_3/Ex_5_3_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void SwapValues(double& x, double& y)
+{
+    double z = x;
+    x = y;
+    y = z;
+}
+
+int main(int argc, char* argv[])
+{
+    double x = 1.0, y = 2.0;
+
+    std::cout<<"BEFORE: x = "<<x<<"\n";
+    std::cout<<"BEFORE: y = "<<y<<"\n";
+
+    SwapValues(x,y);
+    std::cout<<"AFTER: x = "<<x<<"\n";
+    std::cout<<"AFTER: y = "<<y<<"\n";
+}
